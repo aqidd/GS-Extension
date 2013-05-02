5f090d9e4a7a9b8b324a5bfa269691780d2fda7f
Aqid <aa.qidd@gmail.com>
2013-05-02 23:10:41 +0700

    adding indonesian language pack

---
+++ language pack/id_ID.php
@@ -0,0 +1,536 @@
+<?php
+/**
+ * Indonesian Language File
+ *
+ * Date:				4 March 2013
+ * Revision:		March 2011
+ * Version:			GetSimple 3.1
+ * Translators:     Bukhori Muhammad Aqid, 2013-03-04
+ *
+ * @package GetSimple
+ * @subpackage Language
+ */
+
+
+$i18n = array(
+
+/*
+ * For: install.php
+*/
+"PHPVER_ERROR"			=>	"<b>Error:</b> PHP versi 5.1.3 dibutuhkan, versi yang ada ",
+"SIMPLEXML_ERROR"		=>	"<b>Error:</b> <em>SimpleXML</em> tidak didukung oleh server",
+"CURL_WARNING"			=>	"<b>Warning:</b> <em>cURL</em> tidak didukung oleh server",
+"TZ_WARNING"			=>	"<b>Warning:</b> <em>date_default_timezone_set</em> tidak tersedia",
+"WEBSITENAME_ERROR"	=>	"<b>Error:</b> Nama website bermasalah",
+"WEBSITEURL_ERROR"	=>	"<b>Error:</b> URL website bermasalah",
+"USERNAME_ERROR"		=>	"<b>Error:</b> Username tidak diisi dengan benar",
+"EMAIL_ERROR"			=>	"<b>Error:</b> alamat email tidak diisi dengan benar",
+"CHMOD_ERROR"			=>	"<b>Error:</b> Tidak dapat mengatur file konfigurasi. CHMOD 755 atau 777 pada <code>/data</code>, <code>/backups</code> folders &amp; sub-folders dan coba kembali.",
+"EMAIL_COMPLETE"		=>	"Instalasi selesai",
+"EMAIL_USERNAME"		=>	"Username anda adalah",
+"EMAIL_PASSWORD"		=>	"Password anda yang baru adalah",
+"EMAIL_LOGIN"			=>	"Login disini",
+"EMAIL_THANKYOU"		=>	"Terimakasih telah menggunakan",
+"NOTE_REGISTRATION"		=>	"Informasi registrasi anda telah terkirim ke",
+"NOTE_REGERROR"			=>	"<b>Error:</b> Ada masalah dalam pengiriman informasi pendaftaran melalui email. Harap membuat catatan untuk password di bawah ini",
+"NOTE_USERNAME"			=>	"Username anda adalah",
+"NOTE_PASSWORD"			=>	"dan password anda adalah",
+"INSTALLATION"			=>	"Instalasi",
+"LABEL_WEBSITE"			=>	"Nama website",
+"LABEL_BASEURL"			=>	"Website URL",
+"LABEL_SUGGESTION"		=>	"Saran kami adalah",
+"LABEL_USERNAME"		=>	"Username",
+"LABEL_EMAIL"			=>	"Alamat email",
+"LABEL_INSTALL"			=>	"Install sekarang!",
+"SELECT_LANGUAGE"		=> "Pilih bahasa anada",
+"CONTINUE_SETUP" 		=> "Lanjutkan instalasi",
+"DOWNLOAD_LANG" 		=> "Download bahasa",
+
+/* 
+ * For: pages.php
+*/
+"MENUITEM_SUBTITLE"		=>	"menu item",
+"HOMEPAGE_SUBTITLE"		=>	"beranda",
+"PRIVATE_SUBTITLE"		=>	"rahasia",
+"EDITPAGE_TITLE"		=>	"Edit Halaman",
+"VIEWPAGE_TITLE"		=>	"Lihat Halaman",
+"DELETEPAGE_TITLE"		=>	"Hapus Halaman",
+"PAGE_MANAGEMENT"		=>	"Manajemen Halaman",
+"TOGGLE_STATUS"			=>	"Alih Stat<em>u</em>s",
+"TOTAL_PAGES"				=>	"Jumlah halaman",
+"ALL_PAGES"					=>	"Semua halaman",
+
+/* 
+ * For: edit.php
+*/
+"PAGE_NOTEXIST"			=>	"Halaman yang diminta tidak tersedia",
+"BTN_SAVEPAGE"			=>	"Simpan Halaman",
+"BTN_SAVEUPDATES"		=>	"Simpan Perubahan",
+"DEFAULT_TEMPLATE"		=>	"Template Bawaan",
+"NONE"					=>	"Tidak ada",
+"PAGE"					=>	"Halaman",
+"NEW_PAGE"				=>	"Halaman Baru",
+"PAGE_EDIT_MODE"		=>	"Ubah Halaman",
+"CREATE_NEW_PAGE"		=>	"Tambahkan Halaman Baru",
+"VIEW"					=>	"<em>L</em>ihat",
+"PAGE_OPTIONS"			=>	"Opsi Halama<em>n</em>s",
+"SLUG_URL"				=>	"Custom URL (Slug)",
+"TAG_KEYWORDS"			=>	"Tags &amp; Keywords",
+"PARENT_PAGE"			=>	"<i>Page Parent</i>",
+"TEMPLATE"				=>	"Template Halaman",
+"KEEP_PRIVATE"			=>	"Page Visibility",
+"ADD_TO_MENU"			=>	"Tambahkan halaman ini ke dalam menu",
+"PRIORITY"				=>	"Prioritas",
+"MENU_TEXT"				=>	"Teks menu",
+"LABEL_PAGEBODY"		=>	"Konten Halaman",
+"CANCEL"				=>	"Batalkan",
+"BACKUP_AVAILABLE"		=>	"Backup Tersedia",
+"MAX_FILE_SIZE"			=>	"Ukuran file maksimum",
+"LAST_SAVED"			=>	"Halaman terakhir disimpan oleh %s pada",
+"FILE_UPLOAD"			=>	"Upload File",
+"OR"					=>	"atau",
+"SAVE_AND_CLOSE"		=> "Simpan &amp; Tutup",
+
+/*
+ * For: upload.php
+*/
+"ERROR_UPLOAD"			=>	"Ada masalah dengan file yang di<i>upload</i>",
+"FILE_SUCCESS_MSG"		=>	"Sukses! Lokasi file",
+"FILE_MANAGEMENT"		=>	"Manajemen file",
+"UPLOADED_FILES"		=>	"Uploaded Files",
+"SHOW_ALL"				=>	"Lihat semua",
+"VIEW_FILE"				=>	"Lihat File",
+"DELETE_FILE"			=>	"Hapus File",
+"TOTAL_FILES"			=>	": jumlah file &amp; folder",
+
+/*
+ * For: logout.php
+*/
+"MSG_LOGGEDOUT"			=>	"Anda telah keluar dari sistem.",
+
+/* 
+ * For: index.php
+*/
+"LOGIN"					=>	"Login",
+"USERNAME"				=>	"Username",
+"PASSWORD"				=>	"Password",
+"FORGOT_PWD"			=>	"Lupa password anda?",
+"CONTROL_PANEL"			=>	"Control Panel Login",
+
+/*
+ * For: navigation.php
+*/
+"CURRENT_MENU" 			=> 	"Menu Saat ini",
+"NO_MENU_PAGES" 		=> 	"Belum ada halaman yang akan ditampilkan di menu utama",
+
+/*
+ * For: theme-edit.php
+*/
+"TEMPLATE_FILE" 		=> 	"Template file <b>%s</b> telah berhasil diubah!",
+"THEME_MANAGEMENT"		=> 	"Manajemen tampilan",
+"EDIT_THEME" 			=> 	"Ubah tampilan",
+"EDITING_FILE" 			=> 	"Pengubahan File",
+"BTN_SAVECHANGES"		=> 	"Simpan perubahan",
+"EDIT" 					=> 	"Ubah",
+
+/*
+ * For: support.php
+*/
+"SETTINGS_UPDATED"	=> 	"Setting anda telah tersimpan",
+"UNDO"					=> 	"Undo",
+"SUPPORT"				=> 	"Dukungan",
+"SETTINGS"				=>	"Settings",
+"ERROR" 				=> 	"Error",
+"BTN_SAVESETTINGS" 		=> 	"Simpan Settings",
+"VIEW_FAILED_LOGIN"		=> 	"Lihat jumlah login gagal",
+
+
+/* 
+ * For: log.php
+*/
+"MSG_HAS_BEEN_CLR"		=> 	" telah dihapus",
+"LOGS" 					=> 	"Catatan",
+"VIEWING" 				=> 	"Melihat",
+"LOG_FILE" 				=> 	"Log File",
+"CLEAR_ALL_DATA" 		=> 	"Hapus seluruh data",
+"CLEAR_THIS_LOG" 		=> 	"<em>H</em>apus catatan ini",
+"LOG_FILE_ENTRY" 		=> 	"Masukkan Catatan",
+"THIS_COMPUTER"			=>	"Komputer ini",
+
+/*
+ * For: backup-edit.php
+*/
+"BAK_MANAGEMENT"		=>	"Manajemen Backup",
+"ASK_CANCEL"			=>	"<em>B</em>atal", // 'c' is the accesskey identifier
+"ASK_RESTORE"			=>	"<em>K</em>embalikan", // 'r' is the accesskey identifier
+"ASK_DELETE"			=>	"<em>H</em>apus", // 'd' is the accesskey identifier
+"BACKUP_OF"				=>	"Backup of",
+"PAGE_TITLE"			=>	"Judul Halaman",
+"YES"					=>	"Ya",
+"NO"					=>	"Tidak",
+"DATE"					=>	"Tanggal",
+"PERMS"					=>  "Perms",
+
+/*
+ * For: components.php
+*/
+"COMPONENTS"			=>	"Komponen",
+"DELETE_COMPONENT"		=>	"Hapus Komponen",
+"EDIT"					=>	"Ubah",
+"ADD_COMPONENT"			=>	"<em>T</em>ambah Komponen", // 'a' is the accesskey identifier
+"SAVE_COMPONENTS"		=>	"Simpan Komponen",
+
+/*
+ * For: sitemap.php
+*/
+"SITEMAP_CREATED"		=>	"Sitemap Dibuat! Kami juga berhasil ping 4 search engine dari pembaruan",
+"SITEMAP_ERRORPING"		=>	"Sitemap dibuat, namun ada kesalahan ping dari satu atau lebih mesin pencari",
+"SITEMAP_ERROR"			=>	"Sitemap tidak dapat dibuat",
+"SITEMAP_WAIT"			=>	"<b>Harap tunggu:</b> Sedang melakukan pembuatan sitemap",
+
+/*
+ * For: theme.php
+*/
+"THEME_CHANGED"			=>	"Tema telah berhasil diubah",
+"CHOOSE_THEME"			=>	"Pilih tema",
+"ACTIVATE_THEME"		=>	"Aktifkan tema",
+"THEME_SCREENSHOT"	=>	"screenshot tema",
+"THEME_PATH"			=>	"Lokasi folder tema",
+
+/*
+ * For: resetpassword.php
+*/
+"RESET_PASSWORD"		=>	"Reset Password",
+"YOUR_NEW"				=>	"Anda memiliki",
+"PASSWORD_IS"			=>	"password baru : ",
+"ATTEMPT"				=>	"Percobaan",
+"MSG_PLEASE_EMAIL"		=>	"Please enter the username registered on this system, and a new password will be sent to its email address.",
+"SEND_NEW_PWD"			=>	"Kirimkan password baru",
+
+/*
+ * For: settings.php
+*/
+"GENERAL_SETTINGS"		=>	"Pengaturan umum",
+"WEBSITE_SETTINGS"		=>	"Pengaturan website",
+"LOCAL_TIMEZONE"		=>	"Local Timezone",
+"LANGUAGE"				=>	"bahasa",
+"USE_FANCY_URLS"		=>	"Gunakan URL Fancy - <b style=\"font-weight:100\"> Mengharuskan host Anda memiliki <code> mod_rewrite </ code> diaktifkan </ b>",
+"ENABLE_HTML_ED"		=>	"<b>Aktifkan Editor HTML</b>",
+"WARN_EMAILINVALID"		=>	"WARNING: Alamat email anda sepertinya salah!",
+"ONLY_NEW_PASSWORD"		=>	"Silahkan isi kolom apabila anda ingin mengganti password",
+"NEW_PASSWORD"			=>	"Password baru",
+"CONFIRM_PASSWORD"		=>	"Ketik ulang password",
+"PASSWORD_NO_MATCH"		=>	"Passwords tidak sesuai",
+"PERMALINK" 			=> 	"Struktur link kustom",
+"MORE" 					=> 	"detail",
+"HELP" 					=> 	"bantuan",
+
+/*
+ * For: health-check.php
+*/
+"WEB_HEALTH_CHECK"		=>	"Pengecekan stabilitas website",
+"VERSION"				=>	"Versi",
+"UPG_NEEDED"			=>	"Upgrade dibutuhkan ke",
+"CANNOT_CHECK"			=>	"Tidak bisa melakukan pengecekan. Versi yang dimiliki adalah",
+"LATEST_VERSION"		=>	"Sistem dalam kondisi terbaru",
+"SERVER_SETUP"			=>	"Pengaturan awal server",
+"OR_GREATER_REQ"		=>	"atau versi terbaru dibutuhkan",
+"OK"					=>	"OK",
+"INSTALLED"				=>	"Tersedia",
+"NOT_INSTALLED"			=>	"Tidak Tersedia",
+"WARNING"				=>	"Warning",
+"DATA_FILE_CHECK"		=>	"Pengecekan integritas data",
+"DIR_PERMISSIONS"		=>	"Ijin direktori",
+"EXISTANCE"				=>	"%s Eksis",
+"MISSING_FILE"			=>	"File hilang",
+"BAD_FILE"				=>	"File rusak",
+"NO_FILE"				=>	"Tidak ada file",
+"GOOD_D_FILE"			=>	"file 'Deny' baik",
+"GOOD_A_FILE"			=>	"file 'Allow' baik",
+"CANNOT_DEL_FILE"		=>	"Tidak bisa menghapus file",
+"DOWNLOAD"				=>	"Download",
+"WRITABLE"				=>	"Bisa ditulis",
+"NOT_WRITABLE"			=>	"Tidak bisa ditulis",
+
+/*
+ * For: footer.php
+*/
+"POWERED_BY"			=>	"Didukung oleh",
+
+/*
+ * For: backups.php
+*/
+"PAGE_BACKUPS"			=>	"backup halaman",
+"ASK_DELETE_ALL"		=>	"<em>H</em>apus semua",
+"DELETE_ALL_BAK"		=>	"Hapus semua backup?",
+"TOTAL_BACKUPS"			=>	"total backup",
+
+/*
+ * For: archive.php
+*/
+"SUCC_WEB_ARCHIVE"		=>	"Arsip untuk website anda telah berhasil dibuat",
+"SUCC_WEB_ARC_DEL"		=>	"Arsip yang dipilih telah berhasil dihapus",
+"WEBSITE_ARCHIVES"		=>	"Arsip Website",
+"ARCHIVE_DELETED"		=>	"Arsip berhasil dihapus",
+"CREATE_NEW_ARC"		=>	"Buat arsip baru",
+"ASK_CREATE_ARC"		=>	"<em>B</em>uat arsip baru sekarang",
+"CREATE_ARC_WAIT"		=>	"<b>harap tunggu:</b> Sedang membuat arsip baru...",
+"DOWNLOAD_ARCHIVES"		=>	"Download Arsip",
+"DELETE_ARCHIVE"		=>	"Hapus arsip",
+"TOTAL_ARCHIVES"		=>	"Total arsip",
+
+/* 
+ * For: include-nav.php
+*/
+"WELCOME"				=>	"Selamat datang,", // used as 'Welcome USERNAME!'
+"TAB_PAGES"				=>	"<em>H</em>alaman",
+"TAB_FILES"				=>	"B<em>e</em>rkas (gambar & dokumen)",
+"TAB_THEME"				=>	"<em>T</em>ema",
+"TAB_BACKUPS"			=>	"<em>B</em>ackups",
+"PLUGINS_NAV" 			=>  "Plu<em>g</em>in",
+"TAB_SETTINGS"			=>	"<em>P</em>engaturan",
+"TAB_SUPPORT"			=>	"Duku<em>n</em>gan",
+"TAB_LOGOUT"			=>	"<em>L</em>ogout",
+
+/*
+ * For: sidebar-files.php
+*/
+"BROWSE_COMPUTER"		=>	"pilih dari komputer",
+"UPLOAD"				=>	"Upload",
+
+/*
+ * For: sidebar-support.php
+*/
+"SIDE_SUPPORT_LOG"		=>	"Duku<em>n</em>gan",
+"SIDE_HEALTH_CHK"		=>	"Pengecekan <em>K</em>esehatan website",
+"SIDE_DOCUMENTATION"	=>	"Dokumentasi",
+"SIDE_VIEW_LOG"			=>	"<em>L</em>ihat Log",
+
+/*
+ * For: sidebar-theme.php
+*/
+"SIDE_VIEW_SITEMAP"		=>	"<em>L</em>ihat Peta Situs",
+"SIDE_GEN_SITEMAP"		=>	"Generate Site<em>m</em>ap",
+"SIDE_COMPONENTS"		=>	"<em>U</em>bah Komponen",
+"SIDE_EDIT_THEME"		=>	"Ubah <em>t</em>ema",
+"SIDE_CHOOSE_THEME"		=>	"Pilih T<em>e</em>ma",
+
+/*
+ * For: sidebar-pages.php
+*/
+"SIDE_CREATE_NEW"		=>	"<em>B</em>uat Halaman Baru",
+"SIDE_VIEW_PAGES"		=>	"Lihat Semua <em>H</em>alaman",
+
+/*
+ * For: sidebar-settings.php
+*/
+"SIDE_GEN_SETTINGS"		=>	"<em>P</em>engaturan umum",
+"SIDE_USER_PROFILE"		=>	"<em>P</em>rofil pengguna",
+
+/*
+ * For: sidebar-backups.php
+*/
+"SIDE_VIEW_BAK"			=>	"Lihat backup halaman",
+"SIDE_WEB_ARCHIVES"		=>	"<em>A</em>rsip website",
+"SIDE_PAGE_BAK"			=>	"<em>B</em>ackup Halaman",
+
+/*
+ * For: error_checking.php
+*/
+"ER_PWD_CHANGE"			=>	"Jangan lupa untuk <a href=\"settings.php#profile\">mengganti password anda</a> dari password otomatis yang disediakan oleh sistem..",
+"ER_BAKUP_DELETED"		=>	"backup telah dihapus untuk <b>%s</b>",
+"ER_REQ_PROC_FAIL"		=>	"proses yang diminta gagal dieksekusi",
+"ER_YOUR_CHANGES"		=>	"Perubahan pada <b>%s</b> telah berhasil disimpan",
+"ER_HASBEEN_REST"		=>	"<b>%s</b> telah dikembalikan",
+"ER_HASBEEN_DEL"		=>	"<b>%s</b> telah dihapus",
+"ER_CANNOT_INDEX"		=>	"Tidak bisa mengganti alamat URL halaman ini",
+"ER_SETTINGS_UPD"		=>	"Pengaturan anda telah diperbarui",
+"ER_OLD_RESTORED"		=>	"Pengaturan lama anda telah dikembalikan",
+"ER_NEW_PWD_SENT"		=>	"Password baru telah dikirimkan ke alamat email anda",
+"ER_SENDMAIL_ERR"		=>	"Ada masalah dalam pengiriman email. silahkan coba kembali",
+"ER_FILE_DEL_SUC"		=>	"File berhasil dihapus",
+"ER_PROBLEM_DEL"		=>	"Ada masalah dalam penghapusan file",
+"ER_COMPONENT_SAVE"		=>	"Komponen anda telah tersimpan",
+"ER_COMPONENT_REST"		=>	"Komponen anda telah dikembalikan",
+"ER_CANCELLED_FAIL"		=>	"<b>Batal:</b> Proses update dibatalkan",
+
+/*
+ * For: changedata.php
+*/
+"CANNOT_SAVE_EMPTY"		=>	"Tidak dapat menyimpan halaman tanpa judul.",
+"META_DESC" 			=>  "Deskripsi meta.",
+
+/*
+ * For: template_functions.php
+*/
+"FTYPE_COMPRESSED"		=>	"Compressed", //a file-type
+"FTYPE_VECTOR"			=>	"Vector", //a file-type
+"FTYPE_FLASH"			=>	"Flash", //a file-type
+"FTYPE_VIDEO"			=>	"Video", //a file-type
+"FTYPE_AUDIO"			=>	"Audio", //a file-type
+"FTYPE_WEB"				=>	"Web", //a file-type
+"FTYPE_DOCUMENTS"		=>	"Documents", //a file-type
+"FTYPE_SYSTEM"			=>	"System", //a file-type
+"FTYPE_MISC"			=>	"Misc", //a file-type
+"IMAGES"				=>	"Images",
+
+/*
+ * For: login_functions.php
+*/
+"FILL_IN_REQ_FIELD"		=>	"Mohon lengkapi kolom yang masih kosong",
+"LOGIN_FAILED"			=>	"Login gagal, masukkan username dan password yang benar",
+
+/*
+ * For: Date Format
+*/
+"DATE_FORMAT"			=>	"d/m/Y", //please keep short
+"DATE_AND_TIME_FORMAT"  =>	"d F Y, H:i", //date and time
+
+/*
+ * For: welcome.php
+*/
+"WELCOME_MSG"           =>	"Terimakasih telah memilih GetSimple",
+"WELCOME_P"				=>	"GetSimple membuat mengelola website Anda sesederhana mungkin dengan top-of-the-kelas antarmuka pengguna dan sistem termudah template sekitar.",
+"GETTING_STARTED"		=>	"Mulai",
+
+/*
+ * For: image.php
+*/
+
+"CURRENT_THUMBNAIL" 	=> "Current Thumbnail",
+"RECREATE" 				=> "buat ulang",
+"CREATE_ONE" 			=> "buat satu",
+"IMG_CONTROl_PANEL" 	=> "Control Panel untuk Gambar",
+"ORIGINAL_IMG" 			=> "Gambar asli",
+"CLIPBOARD_INSTR" 		=> "Pilih semua",
+"CREATE_THUMBNAIL" 		=> "Buat Thumbnail",
+"CROP_INSTR_NEW" 		=> "<em>ctrl-B</em> atau <em>command-B</em> untuk persegi",
+"SELECT_DIMENTIONS" 	=> "Pemilihan dimensi",
+"HTML_ORIG_IMG" 		=> "Original Image HTML",
+"LINK_ORIG_IMG" 		=> "Original Image Link",
+"HTML_THUMBNAIL" 		=> "Thumbnail HTML",
+"LINK_THUMBNAIL" 		=> "Thumbnail Link",
+"HTML_THUMB_ORIG" 		=> "Thumbnail-to-Image HTML",
+
+/*
+ * For: plugins.php
+*/
+
+"PLUGINS_MANAGEMENT"	=> "Manajemen Plugin",
+"PLUGINS_INSTALLED" 	=> "plugin terinstall",
+"SHOW_PLUGINS"			=> "Plu<em>g</em>in terinstall",
+"PLUGIN_NAME" 			=> "Plugin",
+"PLUGIN_DESC" 			=> "Deskripsi",
+"PLUGIN_VER" 			=> "Versi",
+
+
+
+
+/***********************************************************************************
+ * SINCE Version 3.0
+***********************************************************************************/
+
+/* 
+ * For: setup.php
+ */
+
+"ROOT_HTACCESS_ERROR" 	=> "Gagal untuk membuat .htaccess di root! Harap kopi kode <code>%s</code> ke <code>.htaccess</code> dan ubah <code>%s</code> menjadi <code>%s</code>",
+"REMOVE_TEMPCONFIG_ERROR"	=> "Gagal menghapus <code>%s</code>! Harap lakukan secara manual.",
+"MOVE_TEMPCONFIG_ERROR" => "Gagal mengubah <code>%s</code> menjadi <code>%s</code>! Harap lakukan secara manual.",
+"KILL_CANT_CONTINUE" 	=> "Tidak bisa melanjutkan. Harap selesaikan error terlebih dahulu.",
+"REFRESH" 				=> "Refresh",
+"BETA"					=> "Beta / Bleeding Edge",
+
+/*
+ * Misc Cleanup Work
+ */
+
+# new to 3.0 
+"HOMEPAGE_DELETE_ERROR" => "Tidak bisa menghapus halaman beranda", //deletefile
+"NO_ZIPARCHIVE" => "ekstensi ZipArchive tidak tersedia. Tidak bisa melanjutkan aksi.", //zip
+"REDIRECT_MSG"=> "Apabila browser tidak melakukan redirect, harap klik <a href=\"%s\">disini</a>", //basic
+"REDIRECT"=> "Redirect", //basic
+"DENIED"=> "Ditolak", //sitemap
+"DEBUG_MODE"=> "DEBUG MODE", //nav-include
+"DOUBLE_CLICK_EDIT"=> "Dobel klik untuk melakukan editing", //components
+"THUMB_SAVED"=> "Thumbnail Tersimpan", //image
+"EDIT_COMPONENTS"		=>	"Ubah Komponen", //components
+"REQS_MORE_INFO"=> "Untuk informasi lebih lanjut mengenai modul, kunjungi <a href=\"%s\" target=\"_blank\" >halaman ini</a>.", //install & health-check
+"SYSTEM_UPDATE" => "Update Sistem", // update.php
+"AUTHOR" 				=> "Pembuat", //plugins.php
+"ENABLE" 				=> "Aktifkan", //plugins.php
+"DISABLE" 				=> "Nonaktifkan", //plugins.php
+"NO_THEME_SCREENSHOT" => "Tema anda tidak punya preview", //theme.php
+"UNSAVED_INFORMATION" => "Anda akan meninggalkan halaman ini dan akan kehilangan data yang belum tersimpan.", //edit.php
+"BACK_TO_WEBSITE" => "Kembali ke Website", //index & resetpassword
+"SUPPORT_FORUM" => "Support Forum", //support.php
+"FILTER" => "Filte<em>r</em>", //pages.php
+"UPLOADIFY_BUTTON" => "Upload dokumen dan/atau gambar...", //upload.php
+"FILE_BROWSER" => "Cari dokumen", //filebrowser.php
+"SELECT_FILE" => "Pilih file", //filebrowser.php
+"CREATE_FOLDER" => "Buat Folder", //upload.php
+"THUMBNAIL" => "Thumbnail", //filebrowser.php
+"ERROR_FOLDER_EXISTS" => "Folder yang ingin anda buat sudah ada", //upload.php
+"FOLDER_CREATED" => "Folder baru berhasil dibuat: <b>%s</b>", //upload.php
+"ERROR_CREATING_FOLDER" => "Ada error dalam pembuatan folder baru", //upload.php
+"DELETE_FOLDER" => "Hapus Folder", //upload.php
+"FILE_NAME" => "Nama File", //multiple tr header rows
+"FILE_SIZE" => "ukuran", //multiple tr header rows
+"ARCHIVE_DATE" => "tanggal arsip", //archive.php
+"CKEDITOR_LANG" => "en", // edit.php ; set CKEditor language, don't forget to include CKEditor language file in translation zip
+
+# new to 3.1 
+"XML_INVALID" => "Invalid XML", //template-functions.php
+"XML_VALID" => "Valid XML",
+"UPDATE_AVAILABLE" => "Update ke", //plugins.php
+"STATUS" => "Status", //plugins.php
+"CLONE" => "Duplikat", //edit.php
+"CLONE_SUCCESS" => "Berhasil membuat %s", //pages.php
+"COPY" => "Copy", //pages.php
+"CLONE_ERROR" => "Ada masalah dalam proses duplikasi <b>%s</b>",  //pages.php
+"AUTOSAVE_NOTIFY" => 'halaman tersimpan pada', //edit.php
+"MENU_MANAGER" => '<em>M</em>enu Manager', //edit.php
+"GET_PLUGINS_LINK" => 'Download <em>P</em>lugin lain',
+"SITEMAP_REFRESHED" => "Sitemap situs anda telah diperbarui", //edit.php
+"LOG_FILE_EMPTY" 		=> 	"Tidak ada log file", //log.php
+"SHARE" 		=> 	"Share", //footer.php
+"NO_PARENT" => "No Parent", //edit.php
+"REMAINING" => "karakter tersisa", //edit.php
+"NORMAL" => "Normal", //edit.php
+"ERR_CANNOT_DELETE" => "Tidak bisa menghapus %s. harap lakukan secara manual.", //common.php
+"ADDITIONAL_ACTIONS" => "Aksi lainnya", //edit.php
+"ITEMS" => "item", //upload.php
+"SAVE_MENU_ORDER" => "Simpan susunan menu", //menu-manager.php
+"MENU_MANAGER_DESC" => "Drag-and-drop item menu sekitar sampai Anda memiliki urutan yang Anda inginkan, kemudian klik <strong> 'Simpan menu Order' </ strong> tombol.", //menu-manager.php
+"MENU_MANAGER_SUCCESS" => "Susunan menu berhasil tersimpan", //menu-manager.php
+
+
+/* 
+ * For: api related pages
+ */
+"API_ERR_MISSINGPARAM" => 'parameter data tidak tersedia',
+"API_ERR_BADMETHOD" => 'method %s tidak tersedia',
+"API_ERR_AUTHFAILED" => 'autentikasi gagal',
+"API_ERR_AUTHDISABLED" => 'tidak bisa melakukan autentikasi',
+"API_ERR_NOPAGE" => 'halaman yang diminta %s tidak tersedia',
+"API_CONFIGURATION" => 'konfigurasi API',
+"API_ENABLE" => 'mengaktifkan API',
+"API_REGENKEY" => 'Buat ulang kunci API',
+"API_DISCLAIMER" => "Dengan mengaktifkan API ini Anda sedang mengizinkan aplikasi eksternal yang memiliki salinan kunci Anda untuk memiliki akses ke data website Anda. <b> Hanya berbagi kunci ini dengan aplikasi Anda percaya. </ b>",
+"API_REGEN_DISCLAIMER" => "Bila Anda regenerasi kunci API Anda, Anda akan perlu untuk memasukkan kunci baru ke aplikasi eksternal menggunakan API ini untuk menghubungkan ke situs web Anda.",
+"API_CONFIRM" => "YAKIN?",
+
+
+
+
+"X" => "tidak diterjemahkan",
+
+
+/*
+ * Additions for 3.1
+ */
+"DEBUG_CONSOLE" => 'Konsol Debug'
+
+);
+
+?>
